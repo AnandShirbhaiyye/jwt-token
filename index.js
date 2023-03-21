3e9bd7045f886088c78c12fd99f2c45e1b2d51c9
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-03-21 17:27:45 +0530

    create dotenv file and create login api

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
 const app = express();
-app.use(express.json())
+app.use(express.json());
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = 5000;
+
+
+app.post('/login', async (req, res) => {
+
+    const {email} = req.body;
+    const token = jwt.sign({email: email, isAdmin: true}, process.env.SECREAT_KEYS, {expiresIn: "1min"});
+
+    res.json({
+        success: true,
+        data: {
+            email: email,
+            jwt_token: token
+        },
+        message: "Login  Successfully..."
+    })
+})
 
 
 app.listen(PORT, () => {
