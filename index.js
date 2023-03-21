38cdd354d925f7441e9cd4e7d689dadba954fec0
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-03-21 17:59:56 +0530

    added delete course apu

--- index.js
+++ index.js
@@ -7,23 +7,46 @@ dotenv.config();
 
 const PORT = 5000;
 
+const verifyJwttoken = async (req, res, next) => {
+  const { token } = req.headers;
+  try {
+    const decoded = jwt.verify(token, process.env.SECREAT_KEYS );
+    if (decoded.isAdmin) {
+      next();
+    }
+  } catch (e) {
+    return res.json({
+      success: false,
+      message: e.message,
+    });
+  }
+};
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
+  const { email } = req.body;
+  const token = jwt.sign(
+    { email: email, isAdmin: true },
+    process.env.SECREAT_KEYS,
+    { expiresIn: "1min" }
+  );
 
-    const {email} = req.body;
-    const token = jwt.sign({email: email, isAdmin: true}, process.env.SECREAT_KEYS, {expiresIn: "1min"});
+  res.json({
+    success: true,
+    data: {
+      email: email,
+      jwt_token: token,
+    },
+    message: "login successfully...",
+  });
+});
 
+app.post('/deletecourse',verifyJwttoken, (req, res)=>{
     res.json({
         success: true,
-        data: {
-            email: email,
-            jwt_token: token
-        },
-        message: "Login  Successfully..."
+        message: 'course deleted successfully...'
     })
-})
-
+  })
 
 app.listen(PORT, () => {
-    console.log(`Server running on port number = ${PORT}`)
-})
+  console.log(`Server running on port number = ${PORT}`);
+});
